096e3dcbf468ab9398c37a25c8caa5fec8d6516c
Hadiyal Mahavir <mghadiyal0@gmail.com>
2026-01-31 16:06:10 +0530

    server.change

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,3 +1,5 @@
+
+
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -6,23 +8,23 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 
+const PORT = 5000;
+
 dotenv.config();
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json("Server is Live");
-});
+app.get('/',(req,res)=>{
+    
+    res.json("Server is Live");
+})
 
-// Connect DB once
 await connectDB();
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
-// ❌ Remove app.listen() in serverless
-// export default app for Vercel
-export default app;
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
